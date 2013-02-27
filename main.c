aec903a7416f8dd6267e7f382df532a5429f04df
Joe Desbonnet <jdesbonnet@gmail.com>
2013-02-27 00:21:05 +0000

    Deep sleep with wakeup, 4µA if Vdd<3V

--- main.c
+++ main.c
@@ -55,80 +55,10 @@ int main(void) {
 	systemInit();
 	uartInit(115200);
 
-	gpioSetValue (1, 8, 0);
-	gpioSetValue (1, 8, 1);
-	gpioSetValue (1, 8, 0);
-	gpioSetValue (1, 8, 1);
-	gpioSetValue (1, 8, 0);
-
-/*
-	// Making this input draws 182µA, output just 61µA
-	gpioSetDir (1,8,INPUT);
-	for (i = 1; i < 10; i++) {
-		// 386µA if set to input(0), 63µA if output (1)
-		gpioSetDir (0,i,0); // 1 = output
-		gpioSetValue (0,i,1); // this seems to make no difference
-	}
-	gpioDisableInternalResistors();
-*/
-
-
   	while (1) {
 
-		// http://knowledgebase.nxp.com/showthread.php?t=187
-		//Disable reset pin functionality by making port 0 pin 0 a GPIO pin:
-		//LPC_IOCON->RESET_PIO0_0 |= 0x01
-		// IOCONFIG base: 0x4004 4000
-		// IOCON_nRESET_PIO0_0   : 0x4004 400C
 
-		// The following conditioning of RESET does appear to yield a saveing of 8µA.
-		// However it will disable RESET and a power cycle is needed to re-enter bootloader.
-
-/*
-		IOCON_nRESET_PIO0_0  = 0x01; // selects 0.0 as GPIO, 0x00 as RESET
-		gpioSetDir(0, 0, 1); // 0.0 as output
-		gpioSetValue (0, 0, 1); // 0.0 HIGH
-*/
-
-		// Want all pins as GPIO, pullups off, output at 0V.
-		IOCON_PIO0_1 = IOCON_PIO0_1_FUNC_GPIO; // pin 24 (also bootloader entry)
-		IOCON_PIO0_2 = IOCON_PIO0_2_FUNC_GPIO; // pin 25
-		IOCON_PIO0_3 = IOCON_PIO0_3_FUNC_GPIO; // pin 26
-		IOCON_PIO0_4 = IOCON_PIO0_4_FUNC_GPIO; // pin 27
-		IOCON_PIO0_5 = IOCON_PIO0_5_FUNC_GPIO; // pin 
-		IOCON_PIO0_6 = IOCON_PIO0_6_FUNC_GPIO; // pin 
-		IOCON_PIO0_7 = IOCON_PIO0_7_FUNC_GPIO; // pin 28
-		IOCON_PIO0_8 = IOCON_PIO0_8_FUNC_GPIO; // pin 1
-		IOCON_PIO0_9 = IOCON_PIO0_9_FUNC_GPIO; // pin 2
-		IOCON_JTAG_TCK_PIO0_10 = IOCON_JTAG_TCK_PIO0_10_FUNC_GPIO;
-		IOCON_JTAG_TDI_PIO0_11 = IOCON_JTAG_TDI_PIO0_11_FUNC_GPIO; 
-		IOCON_JTAG_TMS_PIO1_0 = IOCON_JTAG_TMS_PIO1_0_FUNC_GPIO;	
-		IOCON_JTAG_TDO_PIO1_1 = IOCON_JTAG_TDO_PIO1_1_FUNC_GPIO;
-		IOCON_JTAG_nTRST_PIO1_2 = IOCON_JTAG_nTRST_PIO1_2_FUNC_GPIO; // causes extra 150µA drain?!
-		IOCON_SWDIO_PIO1_3 = IOCON_SWDIO_PIO1_3_FUNC_GPIO;
-		IOCON_PIO1_4 = IOCON_PIO1_4_FUNC_GPIO;
-		IOCON_PIO1_5 = IOCON_PIO1_5_FUNC_GPIO;
-		IOCON_PIO1_6 = IOCON_PIO1_6_FUNC_GPIO; // THIS *SEEMED* MADE ALL THE DIFFERENCE! 4µA
-		IOCON_PIO1_7 = IOCON_PIO1_7_FUNC_GPIO;
-		IOCON_PIO1_8 = IOCON_PIO1_8_FUNC_GPIO;
-		IOCON_PIO1_9 = IOCON_PIO1_9_FUNC_GPIO;
-		IOCON_PIO1_10 = IOCON_PIO1_10_FUNC_GPIO;
-		IOCON_PIO1_11 = IOCON_PIO1_11_FUNC_GPIO;
-
-		for (i = 0; i < 10; i++) {
-			// 386µA if set to input(0), 63µA if output (1)
-			gpioSetDir (0,i,1); // 1 = output
-			gpioSetValue (0,i,1); // this seems to make no difference
-		}
-		for (i = 0; i < 7; i++) {
-			gpioSetDir (1,i,1); // 1 = output
-			gpioSetValue (1,i,1); // this seems to make no difference
-		}
-		for (i = 9; i < 12; i++) {
-			gpioSetDir (1,i,1); // 1 = output
-			gpioSetValue (1,i,1); // this seems to make no difference
-		}
-
+		set_pins();
  		pmuDeepSleep(10);
 		//pmuPowerDown();
 
@@ -150,5 +80,63 @@ int main(void) {
 	}
 
  	 return 0;
+}
+
+void set_pins(void) {
+
+	int i;
+	// http://knowledgebase.nxp.com/showthread.php?t=187
+	//Disable reset pin functionality by making port 0 pin 0 a GPIO pin:
+	//LPC_IOCON->RESET_PIO0_0 |= 0x01
+	// IOCONFIG base: 0x4004 4000
+	// IOCON_nRESET_PIO0_0   : 0x4004 400C
+
+	// The following conditioning of RESET does appear to yield a saveing of 8µA.
+	// However it will disable RESET and a power cycle is needed to re-enter bootloader.
+
+	/*
+	IOCON_nRESET_PIO0_0  = 0x01; // selects 0.0 as GPIO, 0x00 as RESET
+	gpioSetDir(0, 0, 1); // 0.0 as output
+	gpioSetValue (0, 0, 1); // 0.0 HIGH
+	*/
+
+	// Want all pins as GPIO, pullups off, output at 0V.
+	IOCON_PIO0_1 = IOCON_PIO0_1_FUNC_GPIO; // pin 24 (also bootloader entry)
+	IOCON_PIO0_2 = IOCON_PIO0_2_FUNC_GPIO; // pin 25
+	IOCON_PIO0_3 = IOCON_PIO0_3_FUNC_GPIO; // pin 26
+	IOCON_PIO0_4 = IOCON_PIO0_4_FUNC_GPIO; // pin 27
+	IOCON_PIO0_5 = IOCON_PIO0_5_FUNC_GPIO; // pin 
+	IOCON_PIO0_6 = IOCON_PIO0_6_FUNC_GPIO; // pin 
+	IOCON_PIO0_7 = IOCON_PIO0_7_FUNC_GPIO; // pin 28
+	IOCON_PIO0_8 = IOCON_PIO0_8_FUNC_GPIO; // pin 1
+	IOCON_PIO0_9 = IOCON_PIO0_9_FUNC_GPIO; // pin 2
+	IOCON_JTAG_TCK_PIO0_10 = IOCON_JTAG_TCK_PIO0_10_FUNC_GPIO;
+	IOCON_JTAG_TDI_PIO0_11 = IOCON_JTAG_TDI_PIO0_11_FUNC_GPIO; 
+	IOCON_JTAG_TMS_PIO1_0 = IOCON_JTAG_TMS_PIO1_0_FUNC_GPIO;	
+	IOCON_JTAG_TDO_PIO1_1 = IOCON_JTAG_TDO_PIO1_1_FUNC_GPIO;
+	IOCON_JTAG_nTRST_PIO1_2 = IOCON_JTAG_nTRST_PIO1_2_FUNC_GPIO; // causes extra 150µA drain?!
+	IOCON_SWDIO_PIO1_3 = IOCON_SWDIO_PIO1_3_FUNC_GPIO;
+	IOCON_PIO1_4 = IOCON_PIO1_4_FUNC_GPIO;
+	IOCON_PIO1_5 = IOCON_PIO1_5_FUNC_GPIO;
+	IOCON_PIO1_6 = IOCON_PIO1_6_FUNC_GPIO; // THIS *SEEMED* MADE ALL THE DIFFERENCE! 4µA
+	IOCON_PIO1_7 = IOCON_PIO1_7_FUNC_GPIO;
+	IOCON_PIO1_8 = IOCON_PIO1_8_FUNC_GPIO;
+	IOCON_PIO1_9 = IOCON_PIO1_9_FUNC_GPIO;
+	IOCON_PIO1_10 = IOCON_PIO1_10_FUNC_GPIO;
+	IOCON_PIO1_11 = IOCON_PIO1_11_FUNC_GPIO;
+
+	for (i = 0; i < 10; i++) {
+		gpioSetDir (0,i,1); // 1 = output
+		gpioSetValue (0,i,1); // this seems to make no difference
+	}
+	for (i = 0; i < 7; i++) {
+		gpioSetDir (1,i,1); // 1 = output
+		gpioSetValue (1,i,1); // this seems to make no difference
+	}
+	for (i = 9; i < 12; i++) {
+		gpioSetDir (1,i,1); // 1 = output
+		gpioSetValue (1,i,1); // this seems to make no difference
+	}
+
 }
 
