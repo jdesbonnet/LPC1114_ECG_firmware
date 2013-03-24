47eff4fc7c9cccaf078aaa17b1e53ea96326c585
Joe Desbonnet <jdesbonnet@gmail.com>
2013-03-24 16:38:32 +0000

    stub for ECG capture

---
+++ project/commands/cmd_ecg_capture.c
@@ -0,0 +1,30 @@
+/**************************************************************************/
+/*! 
+    @file     cmd_ecg_capture.c
+    @author   Joe Desbonnet, jdesbonnet@gmail.com
+
+    @brief    Code to capture ECG from ADI ADAS1000 ECG AFE.
+
+    @section LICENSE
+
+	TBD
+*/
+/**************************************************************************/
+#include <stdio.h>
+
+#include "projectconfig.h"
+#include "core/cmd/cmd.h"
+#include "commands.h"
+
+
+
+
+/**************************************************************************/
+/*! 
+    Capture ECG.
+*/
+/**************************************************************************/
+void cmd_ecg_capture(uint8_t argc, char **argv)
+{
+	printf ("Place holder for ECG capture.\n");
+}
