5f4ed0c67cafd039832470a3c9ea9f81e706d936
Joe Desbonnet <jdesbonnet@gmail.com>
2013-04-28 17:07:50 +0100

    Implement all ADS1292R commands.

--- project/commands/cmd_ads1x9x_cmd.c
+++ project/commands/cmd_ads1x9x_cmd.c
@@ -10,13 +10,29 @@
 void cmd_ads1x9x_cmd(uint8_t argc, char **argv)
 {
 	int cmd;
-	if (strncmp(argv[0],"START",5)==0) {
-		cmd = CMD_START;
-	} else if (strncmp(argv[0],"STANDBY",7)==0) {
+	if (strncmp(argv[0],"STANDBY",7)==0) {
 		cmd = CMD_STANDBY;
 	} else if (strncmp(argv[0],"WAKEUP",6)==0) {
 		cmd = CMD_WAKEUP;
+	} else if (strncmp(argv[0],"RESET",5)==0) {
+		cmd = CMD_RESET;
+	} else if (strncmp(argv[0],"START",5)==0) {
+		cmd = CMD_START;
+	} else if (strncmp(argv[0],"STOP",4)==0) {
+		cmd = CMD_STOP;
+	} else if (strncmp(argv[0],"OFFSETCAL",9)==0) {
+		cmd = CMD_OFFSETCAL;
+	} else if (strncmp(argv[0],"RDATAC",6)==0) {
+		cmd = CMD_RDATAC;
+	} else if (strncmp(argv[0],"SDATAC",6)==0) {
+		cmd = CMD_SDATAC;
+	} else if (strncmp(argv[0],"RDATA",5)==0) {
+		cmd = CMD_RDATA;
+	} else {
+		printf ("ERROR: unrecognized command\r\n");
 	}
+
+	// RREG, WREG handled as separate commands.
 
 	ads1x9x_command(cmd);
 	
