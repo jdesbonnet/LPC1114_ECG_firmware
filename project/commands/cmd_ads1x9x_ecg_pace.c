95bb1a7bedf39e0e70077d98407cf9482563f00c
Joe Desbonnet <jdesbonnet@gmail.com>
2013-11-20 23:25:13 +0000

    Fix CONFIG2 reg default

--- project/commands/cmd_ads1x9x_ecg_pace.c
+++ project/commands/cmd_ads1x9x_ecg_pace.c
@@ -9,7 +9,7 @@
 #include "parse_hex.h"
 #include "stream_encode.h"
 
-#define USE_FP_MATH
+//#define USE_FP_MATH
 #define USE_INT_MATH
 
 /**
@@ -27,12 +27,13 @@ void cmd_ads1x9x_ecg_pace (uint8_t argc, char **argv)
 	uint8_t buf[12];
 
 	#ifdef USE_FP_MATH
+	
 	double ch1d;
 	double upper_envelope=0; 
 	double d;
 	double baseline_lpf=0; 
 	double rm_mains_lpf=0; 
-	double r; int32_t i_r;
+	double r; 
 	double hp_lpf=500; 
 	#endif
 
@@ -40,7 +41,7 @@ void cmd_ads1x9x_ecg_pace (uint8_t argc, char **argv)
 	int32_t i_baseline_lpf=0;
 	int32_t i_rm_mains_lpf=0;
 	int32_t i_hp_lpf=500*256;
-
+	int32_t i_r;
 
 	// UI LED on to indicate ECG capture in progress
 	setLED(1,1);
@@ -52,11 +53,13 @@ void cmd_ads1x9x_ecg_pace (uint8_t argc, char **argv)
 	// Write to CONFIG1 to set continuous mode 500sps (0x02)
 	ads1x9x_register_write(REG_CH1SET, 0x00);
 
-	// Calibrate offset
-	ads1x9x_command(CMD_OFFSETCAL); 
-
 	// Start conversions
 	ads1x9x_command(CMD_START);
+
+
+	// Calibrate offset
+	ads1x9x_command(CMD_OFFSETCAL); 
+	delay(2000000);
 
 	// Issue read continuous (RDATAC)
 	ads1x9x_command(CMD_RDATAC);
@@ -77,12 +80,24 @@ void cmd_ads1x9x_ecg_pace (uint8_t argc, char **argv)
 
 		ads1x9x_ecg_read (&buf);
 
+
+		// Oscilloscope observation on GREEN_LED: 
+		// from here to end of loop (as of 20 Nov 2013)
+		// ~ 32 µs from here to end off loop (with no outpu)
+		// 900µs when outputing pace data
+	setLED(GREEN_LED,0);
+
 		// Record comprises 24bit status + 2 x 24bit channel data
 		// Status is 1100 + LOFF_STAT[4:0] + GPIO[1:0] + 00000 0000 0000
 		status = ((buf[0]<<8 | buf[1])>>5)&0xff;
 
-		ch1 = (buf[3]<<24 | buf[4]<<16 | buf[5]<<8)/256;
-		ch2 = (buf[6]<<24 | buf[7]<<16 | buf[8]<<8)/256;
+		// TODO: looking as assembler output, each byte is loaded separately
+		// Should be possible to move 3 conseq bytes in buf into a 32 bit var
+		// in one instruction (?)
+		//ch1 = (buf[3]<<24 | buf[4]<<16 | buf[5]<<8)/256;
+		//ch2 = (buf[6]<<24 | buf[7]<<16 | buf[8]<<8)/256;
+		ch1 = (buf[3]<<24 | buf[4]<<16 | buf[5]<<8);
+		ch2 = (buf[6]<<24 | buf[7]<<16 | buf[8]<<8);
 
 		// http://electronics.stackexchange.com/questions/30370/fast-and-memory-efficient-moving-average-calculation
 		// FILT <-- FILT + FF(NEW - FILT)
@@ -91,43 +106,48 @@ void cmd_ads1x9x_ecg_pace (uint8_t argc, char **argv)
 		ch1d = (double)ch1;
 		baseline_lpf += (ch1d-baseline_lpf)/256;
 		rm_mains_lpf += (ch1d-rm_mains_lpf)/16;
+		#else
+		//i_baseline_lpf += (ch1*256 - i_baseline_lpf)/256;
+		//i_rm_mains_lpf += (ch1*256 - i_rm_mains_lpf)/16;
+		i_baseline_lpf += (ch1 - i_baseline_lpf)/256;
+		i_rm_mains_lpf += (ch1 - i_rm_mains_lpf)/16;
 		#endif
-
-		i_baseline_lpf += (ch1*256 - i_baseline_lpf)/256;
-		i_rm_mains_lpf += (ch1*256 - i_rm_mains_lpf)/16;
 
 		// Upper envelope detector
 		#ifdef USE_FP_MATH
 		if (rm_mains_lpf > upper_envelope) {
 			upper_envelope = rm_mains_lpf;
 		} else {
-			/*
-			d = upper_envelope-baseline_lpf;
-			if (d>0 && d < 2048) {
-				d=2048;
-			} else if (d<0 && d>-2048) {
-				d=-2048;
-			}
-			upper_envelope -= d/2048;
-			*/
 			upper_envelope -= (upper_envelope-baseline_lpf)/2048;
 		}
-		#endif
+		#else
 		if (i_rm_mains_lpf > i_upper_envelope) {
 			i_upper_envelope = i_rm_mains_lpf;
 		} else {
 			i_upper_envelope -= (i_upper_envelope-i_baseline_lpf)/2048;
 		}
+		#endif
 
-		// Heart period in sample periods
+		// Number of samples since last ECG QRS (x256 for integer implementation)
 		hp = i - last_qrs;
-		i_hp = (i - last_qrs)*256;
+		i_hp = hp * 256;
 
+		// Ignore anything 100 samples after last QRS: too short for next QRS.
 		if ( hp>100 ) {
 
-			// Where is the ECG signal (after 50Hz removal LPF) in the range of
-			// Q to R range? Expect approx -64 -> 256
+			// r/i_r: where is the ECG signal (after 50Hz removal LPF) in
+			// baseline to R range? For FP implementation 0=baseline, 1=R. 
+			// For integer implementation 0=baseline, 256=R.
+			// In both cases r/i_r can be negative (typically -0.3 for FP)
+			// or -64 for integer.
 
+			// If r/i_r crosses 70% threshold then consider it a 'R' event.
+			// If R event update hp_lpf (time averaged heart period). Also
+			// update last_qrs and output data.
+
+			// Important that data output (and all processing) be accomplished
+			// in significantly under the 2000µs sampling period (500sps) or
+			// ECG data will be lost.
 
 			#ifdef USE_FP_MATH
 			r =  (rm_mains_lpf-baseline_lpf) / (upper_envelope-baseline_lpf);
@@ -143,7 +163,7 @@ void cmd_ads1x9x_ecg_pace (uint8_t argc, char **argv)
 			}
 			#else
 			// i_r 0 = baseline; 256 = upper_envelope and also dip negative
-			// TODO: is long casting necessary?
+			// TODO: is long casting necessary?	
 			i_r = (long)(i_rm_mains_lpf - i_baseline_lpf) / ((long)(i_upper_envelope - i_baseline_lpf)/256);
 			if (i_r > 180) {
 				if ( hp < (i_hp_lpf*14)/10) {
@@ -158,6 +178,7 @@ void cmd_ads1x9x_ecg_pace (uint8_t argc, char **argv)
 			#endif
 		}
 
+	setLED(GREEN_LED,1);
 
 		cmdPoll();
 
